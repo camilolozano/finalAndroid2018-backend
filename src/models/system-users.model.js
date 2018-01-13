6e0222e2f9420803fd287e7ab28e40d5d04f5664
Mario Riascos <mariofernando07@hotmail.com>
2018-01-13 13:02:55 -0500

    user code allownull true in systemUser model

--- src/models/system-users.model.js
+++ src/models/system-users.model.js
@@ -11,7 +11,7 @@ module.exports = function (sequelize, DataTypes) {
       },
       userCode: {
         type: DataTypes.STRING(30),
-        allowNull: false,
+        allowNull: true,
         unique: true
       },
       firstName: {
