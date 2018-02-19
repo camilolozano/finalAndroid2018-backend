095674aeffe4a7729c139756a6cbb5b145a1ef9f
sona <camiloes14@gmail.com>
2018-02-18 20:50:05 -0500

    seeder usuarios app

---
+++ src/seeders/9-app-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('appUsers', [{
+        firstNameUser: 'Admin',
+        lastNameUser: 'Admin2',
+        addressUser: 'Big App',
+        contactUser: '7216212',
+        emailUsername: 'admin@admin.com',
+        password: '$2a$08$c23.I3q4DQKQ4621hW7DzuuggtITuqm7uWWVCvmR0Vt63WM1LRs62',
+        state: true
+      }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
